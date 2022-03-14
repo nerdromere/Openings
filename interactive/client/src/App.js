ae1ee18fca5291a2877745c57b0c7ad9a9899645
nerdromere <nerdromere@gmail.com>
2022-03-13 22:23:28 -0700

    finish ui, ready to use

--- interactive/client/src/App.js
+++ interactive/client/src/App.js
@@ -1,75 +1,70 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Col, Container, FormControl, InputGroup, Row } from "react-bootstrap";
+import axios from "axios";
 
 function App() {
 
-  const [openings, setOpenings] = useState([
-    {
-      fen: "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR",
-      file: [
-        { 
-          link: "https://www.youtube.com/watch?v=6IegDENuxU4&t=36s",
-          addedBy: "MrEremenko",
-          dateAdded: "2022-03-10T08:23:53.428Z"
-        },
-        { 
-          link: "https://www.youtube.com/watch?v=6IegDENuxU4&t=36s",
-          addedBy: "MrEremenko",
-          dateAdded: "2022-03-10T08:23:53.428Z"
-        },
-        { 
-          link: "https://www.youtube.com/watch?v=6IegDENuxU4&t=36s",
-          addedBy: "MrEremenko",
-          dateAdded: "2022-03-10T08:23:53.428Z"
-        },
-        { 
-          link: "https://www.youtube.com/watch?v=6IegDENuxU4&t=36s",
-          addedBy: "MrEremenko",
-          dateAdded: "2022-03-10T08:23:53.428Z"
-        },
-        { 
-          link: "https://www.youtube.com/watch?v=6IegDENuxU4&t=36s",
-          addedBy: "MrEremenko",
-          dateAdded: "2022-03-10T08:23:53.428Z"
-        },
-        { 
-          link: "https://www.youtube.com/watch?v=6IegDENuxU4&t=36s",
-          addedBy: "MrEremenko",
-          dateAdded: "2022-03-10T08:23:53.428Z"
-        },
-        { 
-          link: "https://www.youtube.com/watch?v=6IegDENuxU4&t=36s",
-          addedBy: "MrEremenko",
-          dateAdded: "2022-03-10T08:23:53.428Z"
-        },
-        { 
-          link: "https://www.youtube.com/watch?v=6IegDENuxU4&t=36s",
-          addedBy: "MrEremenko",
-          dateAdded: "2022-03-10T08:23:53.428Z"
-        },
-        { 
-          link: "https://www.youtube.com/watch?v=6IegDENuxU4&t=36s",
-          addedBy: "MrEremenko",
-          dateAdded: "2022-03-10T08:23:53.428Z"
-        },
-        { 
-          link: "https://www.youtube.com/watch?v=6IegDENuxU4&t=36s",
-          addedBy: "MrEremenko",
-          dateAdded: "2022-03-10T08:23:53.428Z"
-        },
-        { 
-          link: "https://www.youtube.com/watch?v=6IegDENuxU4&t=36s",
-          addedBy: "MrEremenko",
-          dateAdded: "2022-03-10T08:23:53.428Z"
-        },
-      ]
-    }
-  ]);
-
+  const [openings, setOpenings] = useState([]);
   const [chosen, setChosen] = useState(0);
+
+  const [fen, setFen] = useState('');
+  const [link, setLink] = useState('');
+  const [username, setUsername] = useState('');
+
+  const [error, setError] = useState('');
+
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios.get("/openings/all")
+    .then(response => {
+      // console.log(response.data.openings);
+      setOpenings(response.data.openings);
+    })
+    .catch(err => {
+      console.log("Error getting openings...");
+    });
+  }, []);
 
   const onClickOpening = (e) => {
     setChosen(+e.target.getAttribute("value"));
+  }
+
+  const addOpening = (e) => {
+    if(!/[prnbqkPRNBQK12345678]{0,8}\/[prnbqkPRNBQK12345678]{0,8}\/[prnbqkPRNBQK12345678]{0,8}\/[prnbqkPRNBQK12345678]{0,8}\/[prnbqkPRNBQK12345678]{0,8}\/[prnbqkPRNBQK12345678]{0,8}\/[prnbqkPRNBQK12345678]{0,8}\/[prnbqkPRNBQK12345678]{0,8}/.test(fen)) {
+      setError("Partial FEN not valid");
+    } else if(link.trim().length === 0) {
+      setError("Must add link");
+    } else if(username.trim().length === 0) {
+      setError("Add your github username")
+    } else {
+      setError("");
+      axios.post("/openings/add", {
+        fen,
+        link,
+        username
+      })
+      .then(response => {
+        //if it already existed
+        var index = -1;
+        if(openings.filter(open => open.fen === response.data.fen)) {
+          setOpenings(openings.map((open, idx) => {
+            if(open.fen !== response.data.fen)
+              return open;
+            index = idx;
+            return response.data;
+          }))
+        } else {
+          setOpenings([...openings, response.data]);
+          index = openings.length;
+        }
+        setChosen(index);
+        // setOpenings(response.data.openings);
+      })
+      .catch(err => {
+        console.log("Error getting openings...");
+      });
+    }
   }
 
   return (
@@ -80,6 +75,11 @@ function App() {
               placeholder="Partial FEN (e.g.: rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR)"
               aria-label="Partial FEN"
               aria-describedby="basic-addon1"
+              onChange={e => {
+                setError("");
+                setFen(e.target.value);
+              }}
+              value={fen}
             />
           </InputGroup>
           <InputGroup style={{ marginTop: "2vh" }}>
@@ -87,35 +87,51 @@ function App() {
               placeholder="Link with timestamp to video (e.g.: https://youtu.be/6IegDENuxU4?t=36)"
               aria-label="Username"
               aria-describedby="basic-addon1"
+              onChange={e => setLink(e.target.value)}
+              value={link}
             />
           </InputGroup>
           <InputGroup style={{ marginTop: "2vh" }}>
             <FormControl
-              placeholder="Github Username, possible links will be generated from this"
+              placeholder="Github Username, used to accredit"
               aria-label="Github Username"
               aria-describedby="basic-addon1"
+              onChange={e => setUsername(e.target.value)}
+              value={username}
             />
           </InputGroup>
-          <Button variant="outline-success" style={{ margin: "2vh auto", width: "20vw" }}>Add</Button>
+          <div style={{ color: "red", textAlign: "center" }}>{error.length > 0 ? error: <br />}</div>
+          <Button onClick={addOpening} variant="outline-success" style={{ margin: "2vh auto", width: "20vw" }}>Add</Button>
       </Container>
       <Container style={{ display: "flex", height: "70vh" }}>
-            <Container className="shadow-lg p-3 mb-5 bg-body rounded" style={{ overflowY: "auto", height: "100%", minWidth: "400px", maxWidth: "30vw", marginRight: "1vw", borderStyle: "solid", borderWidth: "0.5px", borderColor: "gray" }}>
+          <Container style={{ display: "flex", flexDirection: "column", height: "70vh" }} fluid={true}>
+            <InputGroup className="pb-3" style={{ marginTop: "2vh" }}>
+              <FormControl
+                placeholder="Search for opening"
+                aria-label="FENSearch"
+                aria-describedby="basic-addon1"
+                onChange={e => setSearch(e.target.value)}
+                value={search}
+              />
+            </InputGroup>
+            <Container fluid={true} className="shadow-lg p-3 mb-3 bg-body rounded" style={{ overflowY: "auto", height: "100%", marginRight: "1vw", borderStyle: "solid", borderWidth: "0.5px", borderColor: "gray" }}>
               {
-                openings.map((opening, idx) => {
-                  return <div key={idx} value={idx} style={{ cursor: "pointer", textDecoration: "underline", fontWeight: chosen === idx ? "bold" : "normal" }}
-                  onClick={onClickOpening}>{opening.fen}</div>
+                openings.length > 0 && openings.filter(opening => opening.fen.includes(search)).map((opening, idx) => {
+                  return <div key={idx} value={idx} 
+                  onClick={onClickOpening}>{chosen === idx ? "> ": ""}<span value={idx}  style={{ cursor: "pointer", textDecoration: "underline" }}>{opening.fen}</span></div>
                 })
               }
             </Container>
+        </Container>
             <Container style={{ display: "flex", flexDirection: "column", alignContent: "center" }}>
-              <img style={{ height: "35vh", margin: "2vh auto" }} src={`https://www.chess.com/dynboard?fen=${openings[chosen].fen}&board=green&piece=neo&size=3`}/>
-              <Container className="shadow-lg bg-body rounded overflow-auto" style={{ overflowY: "auto", height: "100%", minWidth: "400px", maxWidth: "30vw", marginLeft: "1vw", borderStyle: "solid", borderWidth: "0.5px", borderColor: "gray" }}>
+            {openings.length > 0 && <img style={{ height: "35vh", margin: "2vh auto" }} src={`https://www.chess.com/dynboard?fen=${openings[chosen].fen}&board=green&piece=neo&size=3`}/> }
+              <Container className="shadow-lg bg-body rounded overflow-auto" style={{ overflowY: "auto", height: "100%", minWidth: "400px", maxWidth: "30vw", margin: "1vw auto", borderStyle: "solid", borderWidth: "0.5px", borderColor: "gray" }}>
                 <hr/>
-                { openings[chosen].file.map(link => {
-                  return <div>
+                {openings.length > 0 && openings[chosen].file.map((link,idx) => {
+                  return <div key={idx}>
                     <div>Link: {link.link}</div>
-                    <div>Date: {link.addedBy}</div>
-                    <div>Added By: {link.dateAdded}</div>
+                    <div>Added By: {link.addedBy}</div>
+                    <div>Date Added: {link.dateAdded}</div>
                     <hr/>
                   </div>
                 }) }
--- interactive/server/routes/openings.js
+++ interactive/server/routes/openings.js
@@ -1,15 +1,55 @@
 var express = require('express');
 var router = express.Router();
 const fs = require("fs");
+const { request } = require('http');
+const path = require("path");
 
 
-router.get('/get', (req, res, next) => {
-  return res.status(200).json({ test: "whatever" });
+router.get('/all', (req, res, next) => {
+  const files = fs.readdirSync("../../openings");
+  var build = [];
+  files.map(file => {
+    var local = {};
+    local["fen"] = file.replaceAll("-", "/").replace(".json", "");
+    local["file"] = JSON.parse(fs.readFileSync(path.join(__dirname, "../../../openings/" + file)));
+    build.push(local);
+  });
+
+  return res.status(200).json({ openings: build });
 });
 
 router.post('/add', (req, res, next) => { 
+  console.log(req.body);
+  
+  //if file exists, read it
+  var build = [];
+  var file = req.body.fen.replaceAll("/", "-") + ".json";
+  var exists = false;
 
-  return res.status(200).json({  });
+  try {
+    fs.statSync(path.join(__dirname, "../../../openings/" + file))
+    exists = true;
+  } catch(err) {
+    exists = false;
+  }
+  
+  if(exists) {
+    console.log("Such a file exists...");
+    build = JSON.parse(fs.readFileSync(path.join(__dirname, "../../../openings/" + file)));
+    console.log("file contents now is var")
+    console.log(build);
+  }
+
+  //now add onto it
+  build.push({ 
+    "link": req.body.link,
+    "dateAdded": new Date().toISOString(),
+    "addedBy": req.body.username
+  });
+
+  fs.writeFileSync(path.join(__dirname, "../../../openings/" + file), JSON.stringify(build, null, 2));
+
+  return res.status(200).json({ fen: req.body.fen, file: build });
 });
 
 
